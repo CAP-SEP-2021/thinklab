652bad8a8c5b45ee16775b576f044d7a4c60264f
lorenzPatrick <62836542+lorenzPatrick@users.noreply.github.com>
2021-06-01 20:39:25 +0200

    implemented first css styles for booking-tokendialog and added all languages to the dialog

--- angular/src/app/book-table/components/booking-token-dialog/booking-token-dialog.component.ts
+++ angular/src/app/book-table/components/booking-token-dialog/booking-token-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-public-booking-token-dialog',
     templateUrl: './booking-token-dialog.component.html',
     styleUrls: ['./booking-token-dialog.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class BookingTokenDialogComponent implements OnInit{
     
