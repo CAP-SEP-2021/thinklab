81939c66b5784fa60b10c6e3760364bda63eacb6
mohamadbazkhallouf <mohamadbazkhallouf>
2021-06-30 12:01:58 +0200

    Revert "bug fix"
    This reverts commit bf5a0fdb3931fd10f505c87b9b41ba2eecd78e92.

--- angular/src/app/book-table/components/book-table-dialog/book-table-dialog.component.ts
+++ angular/src/app/book-table/components/book-table-dialog/book-table-dialog.component.ts
@@ -22,12 +22,9 @@ export class BookTableDialogComponent implements OnInit {
     private bookingTokenDialog: MatDialog,
     @Inject(MAT_DIALOG_DATA) dialogData: any
   ) {
-
-    this.date = moment(dialogData.bookingDate).format('LLL');
-    var isoDate = this.formatBookingTime(dialogData.bookingDate);
     this.data = {
       booking: {
-        bookingDate: isoDate,
+        bookingDate: dialogData.bookingDate,
         name: dialogData.name,
         email: dialogData.email,
         assistants: dialogData.assistants,
@@ -36,12 +33,8 @@ export class BookTableDialogComponent implements OnInit {
     };
   }
 
-  ngOnInit(): void {   }
-
-  formatBookingTime(bookingDate : string) : string{
-    var myDate : Date = new Date(bookingDate);
-    myDate.setHours( myDate.getHours() +2 );
-    return myDate.toISOString();
+  ngOnInit(): void {
+    this.date = moment(this.data.booking.bookingDate).format('LLL');
   }
 
   sendBooking(): void {
